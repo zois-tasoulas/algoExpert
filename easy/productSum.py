f8e3c5d2480788e55520f751d6940b28e731a8e8
Zois Tasoulas <zoistas@hotmail.com>
2021-01-01 16:02:03 -0600

    Add Product Sum

---
+++ easy/productSum.py
@@ -0,0 +1,22 @@
+def product_sum(array, level=1):
+    array_sum = 0
+    for element in array:
+        if isinstance(element, int):
+            array_sum += element
+        elif isinstance(element, list):
+            array_sum += product_sum(element, level + 1)
+        else:
+            raise Exception("Wrong type for array element")
+
+    return level * array_sum
+
+
+
+def main():
+    # Expected result 146
+    print(product_sum([1, 2, 3, [4, 5], 6, [7, [8, 9]]]))
+
+
+
+if __name__ == "__main__":
+    main()
