eec7319ccfa985f67d929919ecdf43a9e740c9f5
Zois Tasoulas <zoistas@hotmail.com>
2021-01-10 14:58:33 -0600

    Add First Duplicate Value

---
+++ medium/firstDuplicateValue.py
@@ -0,0 +1,22 @@
+def first_duplicate_value(array):
+    dictionary = dict()
+    for index in range(1, len(array) + 1):
+        dictionary[index] = False
+
+    for element in array:
+        if dictionary[element]:
+            return element
+        dictionary[element] = True
+
+    return -1
+
+
+
+def main():
+    array = [5, 3, 6, 4, 2, 6, 8, 4]
+    print(first_duplicate_value(array))
+
+
+
+if __name__ == "__main__":
+    main()
