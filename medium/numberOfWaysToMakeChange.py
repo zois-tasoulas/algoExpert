15813dd3eaa75bddc9816d384a4f64fd7b53bdf3
Zois Tasoulas <zoistas@hotmail.com>
2021-01-14 23:56:59 -0600

    number Of Ways To Make Change

---
+++ medium/numberOfWaysToMakeChange.py
@@ -0,0 +1,23 @@
+def number_of_ways_to_make_change(n, denominations):
+    ways_per_number = dict()
+    ways_per_number[0] = 1
+    for ii in range(1, n + 1):
+        ways_per_number[ii] = 0
+
+    for element in denominations:
+        for ii in range(element, n + 1):
+            ways_per_number[ii] += ways_per_number[ii - element]
+
+    return ways_per_number[n]
+
+
+
+def main():
+    n = 11
+    denominations = [1, 25, 5, 10]
+    print(number_of_ways_to_make_change(n, denominations))
+
+
+
+if __name__ == "__main__":
+    main()
